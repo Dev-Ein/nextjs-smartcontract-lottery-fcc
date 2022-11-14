a9504f818b0232840f5e58e13c9d2da1a5f6ab90
EINCORE <eincore@github.com>
2022-11-14 12:49:23 +0100

    initial-commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+// Import works with our front end
+// require does not 
+// nodes != ecmascript / javascript
+// backendJS is a litle different from front end
+
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+// import ManualHeader from "../components/ManualHeader"
+import Header from "../components/Header"
+import LotteryEntrance from "../components/LotteryEntrance"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our Smart Contract Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <ManualHeader /> */}
+      <Header/>
+      <LotteryEntrance/>
+      Hello!
+    </div>
+  )
+}
